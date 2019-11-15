4e008d950396d93b4050f13439b640b47d2da95f
Eddy <adnansee@gmail.com>
2019-11-15 13:15:23 +0100

    Initial commit

---
+++ src/test/java/com/example/herokutry/HerokutryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.herokutry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HerokutryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
